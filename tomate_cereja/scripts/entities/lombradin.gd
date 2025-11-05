1d71f8b2ba9074838d758ea1093ca39b659d9f7f
Gabriel Spinola <gabrielspinola77@gmail.com>
2025-11-05 16:59:09 -0300

    Update lombradin.gd

--- tomate_cereja/scripts/entities/lombradin.gd
+++ tomate_cereja/scripts/entities/lombradin.gd
@@ -5,13 +5,21 @@ const GameLayers = preload("res://scripts/systems/game_layers.gd")
 var _gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @export_group("Movement")
-@export var _speed := 5.
+@export var _walk_speed := 5.
+@export var _run_speed := 5.
 @export var _friction := 5.
+
+var speed = .0
 
 @export_group("Camera")
 @export_range(.01, 1) var _sens := 0.5
+@export_range(1, 3) var _bob_frequency := 2.0
+@export_range(.01, .2) var _bob_amplitude := 0.08
+
 @onready var head = $Head
 @onready var _pcamera = $Head/PhantomCamera3D
+
+var _t_bob := .0
 
 const _MIN_PITCH: float = -40
 const _MAX_PITCH: float = 60
@@ -30,20 +38,29 @@ func _physics_process(delta: float) -> void:
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
+	var is_running = Input.is_action_pressed("sprint")
+
 	if direction:
-		velocity.x = direction.x * _speed 
-		velocity.z = direction.z * _speed 
+		velocity.x = direction.x * (if (is_running) _run_speed else walk_speed)
+		velocity.z = direction.z * _walk_speed 
 	else:
 		velocity.x = move_toward(velocity.x, 0, _friction )
 		velocity.z = move_toward(velocity.z, 0, _friction )
 
+	_t_bob += delta * velocity.length() * float(is_on_floor())
+	_pcamera.transform.origin = _headbob(_t_bob)
+
 	move_and_slide()
 
 func _handleCamera(event: InputEvent):
-	if not event is InputEventMouse:
-		return
-	
-	head.rotate_y(-event.relative.x * _sens * 0.01)
+	if event is InputEventMouse:
+		head.rotate_y(-event.relative.x * _sens * 0.01)
+		_pcamera.rotate_x(-event.relative.y * _sens * 0.01)
+		_pcamera.rotation.x = clamp(_pcamera.rotation.x, deg_to_rad(_MIN_PITCH), deg_to_rad(_MAX_PITCH))
 
-	_pcamera.rotate_x(-event.relative.y * _sens * 0.01)
-	_pcamera.rotation.x = clamp(_pcamera.rotation.x, deg_to_rad(_MIN_PITCH), deg_to_rad(_MAX_PITCH))
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * _bob_frequency) * _bob_amplitude
+	pos.x = sin(time * _bob_frequency / 2) * _bob_amplitude
+
+	return pos
